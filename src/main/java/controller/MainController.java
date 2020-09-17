e0228555789fb49cfd89361949369c87bd83cf86
Reivilo85k <laborde.olivier@gmail.com>
2020-09-17 10:51:23 +0300

    fixed path issues in the code due to reorganization of the fxml folders, created search methods in controller and linked them to fxml files

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -6,13 +6,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+
 public class MainController {
 
     @FXML
     AnchorPane maindisplay;
     public void showCreateBook(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/showCreateBook.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/CreateEntities/showCreateBook.fxml"));
             maindisplay.getChildren().setAll(root);
         }catch (Exception e){
             e.printStackTrace();
@@ -21,7 +23,7 @@ public class MainController {
 
     public void showCreateAuthor(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/showCreateAuthor.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/CreateEntities/showCreateAuthor.fxml"));
             maindisplay.getChildren().setAll(root);
         }catch (Exception e){
             e.printStackTrace();
@@ -30,7 +32,7 @@ public class MainController {
 
     public void showCreateUser(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/showCreateUser.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/CreateEntities/showCreateUser.fxml"));
             maindisplay.getChildren().setAll(root);
         }catch (Exception e){
             e.printStackTrace();
@@ -39,18 +41,60 @@ public class MainController {
 
     public void showCreateReview(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/showCreateReview.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/CreateEntities/showCreateReview.fxml"));
             maindisplay.getChildren().setAll(root);
         }catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public void showCreateRentals(ActionEvent actionEvent) {
+    public void showCreateRental(ActionEvent actionEvent) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("/showCreateRentals.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/CreateEntities/showCreateRental.fxml"));
             maindisplay.getChildren().setAll(root);
         }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void showSearchBook(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/SearchEntities/showSearchBook.fxml"));
+            maindisplay.getChildren().setAll(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void showSearchAuthor(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/SearchEntities/showSearchAuthor.fxml"));
+            maindisplay.getChildren().setAll(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showSearchUser(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/SearchEntities/showSearchUser.fxml"));
+            maindisplay.getChildren().setAll(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showSearchReview(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/SearchEntities/showSearchReview.fxml"));
+            maindisplay.getChildren().setAll(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void showSearchRental(ActionEvent actionEvent) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/SearchEntities/showSearchRental.fxml"));
+            maindisplay.getChildren().setAll(root);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
